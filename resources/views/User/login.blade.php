5a3c2d57ccbc2e8ad917629b7cd1c22308b623ab
fifaatade <fifaatade2020@gmail.com>
2024-03-08 11:56:13 +0100

    review login page

--- resources/views/User/login.blade.php
+++ resources/views/User/login.blade.php
@@ -22,8 +22,8 @@
                 <button type="submit" class="btn btn-success float-end "> Se connecter</button>
             </div>
             <div class="">
-<!--                 <p class="mt-3 me-5"><b>Vous n'avez pas un compte?</b> <a href="{{route('register')}}">cliquez ici</a></p>
- -->                <p class="mt-3  float-start text-danger"><b>Mot de passe oublié? </b><a href="{{route('forgetPassword')}}" class="text-danger">cliquez ici</a></p>
+                 <p class="mt-3 me-5"><b>Vous n'avez pas un compte?</b> <a href="{{route('register')}}">cliquez ici</a></p>
+                 <p class="mt-3  float-start text-danger"><b>Mot de passe oublié? </b><a href="{{route('forgetPassword')}}" class="text-danger">cliquez ici</a></p>
             </div>
         </form>
     
